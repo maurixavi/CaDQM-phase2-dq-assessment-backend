150c732f48a54b39a63b40265a53a7c930b853e2
maurixavi <mxavierarevalo@gmail.com>
2025-01-09 12:06:16 -0300

    Delete dq_model previous django app component

--- dq_model/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DqModelConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dq_model'
--- myproject/urls.py
+++ myproject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,6 @@ urlpatterns = [
     path('api/', include('project.urls')),
     path('api/', include('dqmodel.urls')),
     path('data/', include('metadata.urls')),
+    
+    path('docs/', include_docs_urls(title='API Documentation'))
 ]
